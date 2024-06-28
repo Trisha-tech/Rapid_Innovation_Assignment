f537236c5023b94828bb2e948b4e4ec15d014ff8
Trisha-tech <1234trishasahu@gmail.com>
2024-06-28 16:44:46 +0530

    Mongodb Connection Added

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const server = http.createServer(app);
 
 app.use(bodyParser.json());
 
+const dotenv = require(`dotenv`)
+const mongoose = require('mongoose')
+
+dotenv.config({path : `.env`})
+
+/*MONGODB CONNECTION START*/
+const MONGO_URL = process.env.MONGO_URL ;
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () => {
+    console.log("Connected to MongoDB")
+})
+mongoose.connection.on('error', (err) => {
+    console.log("Error Connecting to Database", err)
+})
+/*MONGODB CONNECTION END*/
+
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
